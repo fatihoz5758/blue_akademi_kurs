e377fd46f9f88330541a09ee47bc1f53e3efcb68
fatih5758 <hestuna@gmail.com>
2023-02-01 20:59:25 +0300

    blue_akademi

---
+++ js.js
@@ -0,0 +1,32 @@
+var isOpen = false;
+function ac_kapa() {
+    if (isOpen == false) {
+        document.getElementById('menu').style.display = 'block';
+        isOpen = true;
+    } else {
+        document.getElementById('menu').style.display = 'none';
+        isOpen = false;
+    }
+}
+
+var isOpen = false;
+function ac_kapa2() {
+    if (isOpen == false) {
+        document.getElementById('menu2').style.display = 'block';
+        isOpen = true;
+    } else {
+        document.getElementById('menu2').style.display = 'none';
+        isOpen = false;
+    }
+}
+
+var isOpen = false;
+function ac_kapa3() {
+    if (isOpen == false) {
+        document.getElementById('menu3').style.display = 'block';
+        isOpen = true;
+    } else {
+        document.getElementById('menu3').style.display = 'none';
+        isOpen = false;
+    }
+}
